af8a90eaa6bfa286640844db689c7994ce9f0711
Seongsangcho <liasqui31@gmail.com>
2021-09-20 22:22:28 +0900

    feat : landing page 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { theme } from "styles/theme";
+import { theme, lightTheme } from "styles/theme";
+import Head from "next/head";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [mode, setMode] = useState(false);
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+      <ThemeProvider theme={mode ? lightTheme : theme}>
+        <Head>
+          <title>BI</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} mode={mode} setMode={setMode} />
       </ThemeProvider>
     </>
   );
